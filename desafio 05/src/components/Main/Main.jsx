4be5fd88dcbe32c584a33915b6ef0940488a070a
PG Paulo <35962308+PauloGerson@users.noreply.github.com>
2023-06-02 12:57:39 -0300

    Update Main.jsx

--- desafio 05/src/components/Main/Main.jsx
+++ desafio 05/src/components/Main/Main.jsx
@@ -12,7 +12,7 @@ export default function Main() {
             <h2>HAYAO MIYAZAKI</h2>
             <h1>A VIAGEM <br /> DE CHIHIRO</h1>
 
-            <p>Chihiro chega a um mundo mágico dominado por  uma bruxa. <br />Aqueles que a desobedecem são  transformados em animais.</p>
+            <p>Chihiro chega a um mundo mágico dominado por  uma bruxa. Aqueles que a desobedecem são  transformados em animais.</p>
 
             <div className="info-btn">
               <button className='btn-01'><BiPlay/>  ASSISTIR AGORA</button>
